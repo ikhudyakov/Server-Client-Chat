eb77590c8ef3d36dd73adb90a7a60ffdc8edaf09
Dan11l <noname.spb@mail.ru>
2018-06-06 00:41:43 +0300

    added class history

--- src/components/History.java
+++ src/components/History.java
@@ -1,20 +1,29 @@
 package components;
 
+import javafx.scene.shape.Path;
 import messages.Messages;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class History {
     public static void saveMessageInFile(int id, Messages msg) throws IOException {
 
-        String path = "history" + id + ".txt";
+        String path = "/history" + id + ".txt";
 
         File file = new File(path);
-            if (file.createNewFile() ){
-                System.out.println(file + " created");
-            } else {
+        if (file.createNewFile()) {
+            System.out.println(file + " created");
+        } else {
 
-            }
+        }
+        String rrr = msg.toString();
+        try {
+            Files.write(Paths.get(file.getPath()), rrr.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
