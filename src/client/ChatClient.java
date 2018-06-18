959dfb132613fa03b6c002f007cb4d24ea6f2d80
Dan11l <noname.spb@mail.ru>
2018-06-18 17:59:03 +0300

    For GUI + Now it doesn't work

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -313,6 +313,11 @@ public class ChatClient extends Application {
 
         //launch(args);
         //ChatServer.main(8080);
+        start(args);
+        //return;
+    }
+
+    private static void start(String[] args) throws IOException, InterruptedException {
         String address = null;
 
         if (args != null && args.length > 0)
@@ -326,6 +331,6 @@ public class ChatClient extends Application {
         }
 
         ChatClient client = new ChatClient(parseAddress(address), scanner);
-        client.start();
+       // client.start();
     }
 }
