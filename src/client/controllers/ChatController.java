2b338152155fcd61f8a97f866e2164cd84ca3e65
Dan11l <noname.spb@mail.ru>
2018-06-26 23:07:12 +0300

    send message

--- src/client/controllers/ChatController.java
+++ src/client/controllers/ChatController.java
@@ -1,5 +1,6 @@
 package client.controllers;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.net.URL;
@@ -7,7 +8,10 @@ import java.util.ResourceBundle;
 
 import client.ChatClient;
 import components.IOUtils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -17,40 +21,33 @@ import static com.sun.java.accessibility.util.AWTEventMonitor.addWindowListener;
 
 public class ChatController {
 
-    @FXML
-    private ResourceBundle resources;
+    @FXML private ResourceBundle resources;
+    @FXML private URL location;
+    @FXML private TextField text_field;
+    @FXML private ImageView immageButtonAttachFile;
+    @FXML private ListView<String> messagesList;
+    @FXML private Button sendButton;
+    @FXML private ListView<String> onlineUsers;
 
-    @FXML
-    private URL location;
-
-    @FXML
-    private TextField text_field;
-
-    @FXML
-    private ImageView immageButtonAttachFile;
-
-    @FXML
-    private ListView<?> messagesList;
-
-    @FXML
-    private Button sendButton;
-
-    @FXML
-    private ListView<?> onlineUsers;
+    ObservableList<String> chatMessages = FXCollections.observableArrayList();
 
     ChatClient chatClient;
 
-    @FXML
-    void initialize() {
+    @FXML void initialize() {
         chatClient = Controller.getChatClient();
 
+        messagesList.setItems(chatMessages);
+
+        sendButton.setOnAction(event -> {
+            chatMessages.add( "USER " + text_field.getText());
+            text_field.setText("");
 
 
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                IOUtils.closeQuietly(chatClient.getSocket());
-                System.exit(0);
-            }
+//        addWindowListener(new WindowAdapter() {
+//            public void windowClosing(WindowEvent e) {
+//                IOUtils.closeQuietly(chatClient.getSocket());
+//                System.exit(0);
+//            }
         });
 
     }
