d061220cf0c5bc4e48b37cb915c7f985b2ff69ab
Ilya <ilykhudyakov@gmail.com>
2018-06-27 00:58:01 +0300

    27.06.2018 +++

--- src/client/controllers/ChatController.java
+++ src/client/controllers/ChatController.java
@@ -21,35 +21,44 @@ import static com.sun.java.accessibility.util.AWTEventMonitor.addWindowListener;
 
 public class ChatController {
 
-    @FXML private ResourceBundle resources;
-    @FXML private URL location;
     @FXML private TextField text_field;
     @FXML private ImageView immageButtonAttachFile;
     @FXML private ListView<String> messagesList;
     @FXML private Button sendButton;
     @FXML private ListView<String> onlineUsers;
+    private String text;
 
     ObservableList<String> chatMessages = FXCollections.observableArrayList();
 
-    ChatClient chatClient;
+    private ChatClient chatClient;
 
     @FXML void initialize() {
         chatClient = Controller.getChatClient();
+//
 
         messagesList.setItems(chatMessages);
 
         sendButton.setOnAction(event -> {
-            chatMessages.add( chatClient + " " + text_field.getText());
-            text_field.setText("");
-
-
-//        addWindowListener(new WindowAdapter() {
-//            public void windowClosing(WindowEvent e) {
-//                IOUtils.closeQuietly(chatClient.getSocket());
-//                System.exit(0);
-//            }
+            sendMes();
         });
 
+        text_field.setOnAction(event -> {
+            sendMes();
+        });
+
+    }
+
+    private void sendMes() {
+        //chatMessages.add( "USER " + text_field.getText());
+        chatClient.sendText(text_field.getText());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        text = chatClient.getText();
+        chatMessages.add(text);
+        text_field.setText("");
     }
 }
 
